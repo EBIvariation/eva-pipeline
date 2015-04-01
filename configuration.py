e8b4a68ca5b7315d16bac867e43b0ca2af84d504
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2015-04-01 14:52:25 +0100

    catalog_folder configuration option no longer required

--- configuration.py
+++ configuration.py
@@ -25,7 +25,6 @@ def get_opencga_config(filename):
     config.read(filename)
 
     root_folder  = config.get('opencga', 'root_folder')
-    catalog_folder = config.get('opencga', 'catalog_folder')
     catalog_host = config.get('opencga', 'catalog_host')
     catalog_port = config.getint('opencga', 'catalog_port')
     catalog_user = config.get('opencga', 'catalog_user')
@@ -35,13 +34,12 @@ def get_opencga_config(filename):
         raise ConfigParser.ParsingError('References to OpenCGA installation not properly configured, '
                                         'please check your {filename} file'.format(filename=filename))
 
-    if not catalog_folder or not catalog_host or not catalog_port or not catalog_user or not catalog_pass:
+    if not catalog_host or not catalog_port or not catalog_user or not catalog_pass:
         raise ConfigParser.ParsingError('References to OpenCGA catalog not properly configured, '
                                         'please check your {filename} file'.format(filename=filename))
 
     return { 
              'root_folder' : root_folder,
-             'catalog_folder' : catalog_folder, 
              'catalog_host' : catalog_host, 
              'catalog_port' : catalog_port,
              'catalog_user' : catalog_user,
