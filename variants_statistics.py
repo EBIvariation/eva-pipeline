7878dfade89104e6a123470a35644e52f7784cd3
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2015-05-05 17:15:43 +0100

    CreateVariantStatistics require and output methods revised, still need completion checks

--- variants_statistics.py
+++ variants_statistics.py
@@ -2,8 +2,7 @@ import os
 import json
 import luigi
 
-from variants_loading import LoadGenotypesFile
-from samples_loading import CreatePopulationCohorts
+from samples_loading import CreateGlobalCohort, CreatePopulationCohorts
 from shellout import shellout_no_stdout, shellout
 import configuration
 
@@ -12,6 +11,7 @@ class CreateVariantsStatistics(luigi.Task):
   
   path = luigi.Parameter()
   database = luigi.Parameter()
+  #index_file_id = luigi.Parameter(default="")
   
   study_alias = luigi.Parameter()
   study_name = luigi.Parameter(default="")
@@ -24,21 +24,17 @@ class CreateVariantsStatistics(luigi.Task):
   project_description = luigi.Parameter(default="")
   project_organization = luigi.Parameter(default="")
   
-  index_file_id = luigi.Parameter(default="")
-  
   def requires(self):
     return [
-             #LoadGenotypesFile(self.path, self.database, self.study_alias, self.study_name, self.study_description, self.study_uri, self.study_ticket_uri,
-             #                  self.project_alias, self.project_name, self.project_description, self.project_organization),
-             #CreatePopulationCohorts(self.study_alias, self.study_name, self.study_description, self.study_uri, self.study_ticket_uri,
-             #                  self.project_alias, self.project_name, self.project_description, self.project_organization)
+             CreateGlobalCohort(self.study_alias, self.study_name, self.study_description, self.study_uri, self.study_ticket_uri,
+                              self.project_alias, self.project_name, self.project_description, self.project_organization),
+             CreatePopulationCohorts(self.study_alias, self.study_name, self.study_description, self.study_uri, self.study_ticket_uri,
+                              self.project_alias, self.project_name, self.project_description, self.project_organization)
            ]
 
   def get_cohort_ids(self):
-
     config = configuration.get_opencga_config('pipeline_config.conf')
 
-    # TODO Retrieve the number of possible values for variable 'Population'
     command = '{opencga-root}/bin/opencga.sh studies info --user {user} --password {password} ' \
               '--study-id "{user}@{project-alias}/{study-alias}" > {output}'
     kwargs = {'opencga-root'    : config['root_folder'],
@@ -61,32 +57,80 @@ class CreateVariantsStatistics(luigi.Task):
       study_cohorts = study_json['cohorts']
 
     # Get the ids of all the cohorts created for the study
-    cohorts_ids = []
-    for cohort in study_cohorts:
-      cohorts_ids.append(cohort['id'])
-    
+    cohorts_ids = [ str(cohort['id']) for cohort in study_cohorts ]
     return cohorts_ids
 
 
   def run(self):
     config = configuration.get_opencga_config('pipeline_config.conf')
-    # 
     command = '{opencga-root}/bin/opencga.sh files stats-variants --user {user} --password {password} ' \
-              '--cohort-id {cohort_id} --file-id {index_file_id} ' \
+              '--cohort-id {cohort-id} --file-id "{user}@{project-alias}/{study-alias}/40_transformed/{filename}.MONGODB" ' \
               '--outdir-id "{user}@{project-alias}/{study-alias}/50_stats/" -- --create '
     kwargs = {'opencga-root'    : config['root_folder'],
               'user'            : config['catalog_user'],
               'password'        : config['catalog_pass'],
               'project-alias'   : self.project_alias,
               'study-alias'     : self.study_alias,
-              'cohort_id'       : str(self.get_cohort_ids()).replace(" ", "")[1:-1],
-              'index_file_id'   : self.index_file_id}
+              'cohort-id'       : ",".join(self.get_cohort_ids()),
+              'filename'        : os.path.basename(self.path)}
     
     shellout_no_stdout(command, **kwargs)
     
-  
+
   def complete(self):
     return False
+
+
+  def output(self):
+    """
+    The output files are json.gzip containing variants and files stats, and searched in the folder specified in OpenCGA Catalog
+    """
+    
+    project_id = -1
+    study_id = -1
+    config = configuration.get_opencga_config('pipeline_config.conf')
+    
+    # Get project numerical ID
+    command = '{opencga-root}/bin/opencga.sh projects info --user {user} --password {password} ' \
+              '-id "{user}@{project-alias}" --output-format IDS > {output}'
+    kwargs = {'opencga-root'    : config['root_folder'],
+              'user'            : config['catalog_user'],
+              'password'        : config['catalog_pass'],
+              'project-alias'   : self.project_alias,
+              'output'          : "/tmp/" + os.path.basename(self.path) + ".project"}
+    
+    project_output = shellout(command, **kwargs)
+    if os.path.getsize(project_output) > 0:
+      with open(project_output, 'r') as file:
+        project_id = int(file.read())
+    
+    # Get study numerical ID and data folder
+    command = '{opencga-root}/bin/opencga.sh studies info --user {user} --password {password} ' \
+              '-id "{user}@{project-alias}/{study-alias}" > {output}'
+    kwargs = {'opencga-root'    : config['root_folder'],
+              'user'            : config['catalog_user'],
+              'password'        : config['catalog_pass'],
+              'project-alias'   : self.project_alias,
+              'study-alias'     : self.study_alias,
+              'output'          : "/tmp/" + os.path.basename(self.path) + ".study"}  
+    
+    study_output = shellout(command, **kwargs)
+    if os.path.getsize(study_output) > 0:
+      with open(study_output, 'r') as file:
+        study_json = json.load(file)
+        study_id = study_json['id']
+        study_folder = study_json['uri'].split(':', 1)[1]  # Remove prefix "file:"
+    
+    #files_root = "{folder}/50_stats/{filename}".format(folder=study_folder, filename=os.path.basename(self.path))
+    files_root = "{folder}/50_stats/stats_".format(folder=study_folder)
+    
+    # TODO Retrieve a list of all the cohorts
+    # TODO Append list of underscore-separated cohort names
+    # TODO Traverse 50_stats folder, looking for files that match that pattern (other list of cohorts mean it is not done)
+    # TODO Get the newest files, whose date fragment will be the largest number
+    
+    return { 'variants' : luigi.LocalTarget(files_root + '.variants.stats.json.gz'),
+             'file'     : luigi.LocalTarget(files_root + '.source.stats.json.gz') }
  
 
 
