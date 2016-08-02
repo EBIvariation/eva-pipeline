2c44d2610ebaf260407727052e0a17a9af3c16d5
Diego Poggioli <diego@ebi.ac.uk>
2016-08-02 16:20:08 +0100

    jobLauncherTestUtils bean removed

--- src/test/java/embl/ebi/variation/eva/pipeline/jobs/AnnotationConfig.java
+++ src/test/java/embl/ebi/variation/eva/pipeline/jobs/AnnotationConfig.java
@@ -16,7 +16,6 @@
 
 package embl.ebi.variation.eva.pipeline.jobs;
 
-import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -30,13 +29,10 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @Configuration
 @PropertySource({"annotation.properties"})
 public class AnnotationConfig {
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
-    @Bean
-    public JobLauncherTestUtils jobLauncherTestUtils() {
-                return new JobLauncherTestUtils();
-           }
 }
