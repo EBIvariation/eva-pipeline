640a86ce95f832672e78cfc252e438df4f0f433d
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2016-09-09 12:56:44 +0100

    Entry point class Application moved to package 'uk.ac.ebi.eva.pipeline'

--- src/main/java/uk/ac/ebi/eva/pipeline/Application.java
+++ src/main/java/uk/ac/ebi/eva/pipeline/Application.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.ac.ebi.eva;
+package uk.ac.ebi.eva.pipeline;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
