3a134ec511babf94490cc296f47867c9a6e335a9
Diego Poggioli <diego@ebi.ac.uk>
2016-10-17 12:00:36 +0100

    Extra checks added to verify that Pedigree fields are populated properly

--- src/test/java/uk/ac/ebi/eva/pipeline/jobs/steps/PedLoaderStepTest.java
+++ src/test/java/uk/ac/ebi/eva/pipeline/jobs/steps/PedLoaderStepTest.java
@@ -1,8 +1,13 @@
 package uk.ac.ebi.eva.pipeline.jobs.steps;
 
+import com.google.common.collect.Sets;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.opencb.biodata.models.pedigree.Condition;
+import org.opencb.biodata.models.pedigree.Individual;
+import org.opencb.biodata.models.pedigree.Pedigree;
+import org.opencb.biodata.models.pedigree.Sex;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -14,7 +19,10 @@ import org.springframework.test.util.ReflectionTestUtils;
 import uk.ac.ebi.eva.pipeline.configuration.CommonConfiguration;
 import uk.ac.ebi.eva.pipeline.configuration.JobOptions;
 
+import java.util.stream.Collectors;
+
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Diego Poggioli
@@ -22,10 +30,9 @@ import static junit.framework.TestCase.assertTrue;
  * Test for {@link PedLoaderStep}
  *
  * Using reflections to inject jobOptions into the tasklet. There are other ways to do this but a been is required.
- * Otherewise the tasklet needs to be into a Job. See:
- * http://docs.spring.io/spring-batch/reference/html/testing.html
- * 10.4 Testing Step-Scoped Components
  *
+ * TODO: This could be simplified by using StepRunner. That requires using JobParametersinstead of our ownJobOptions
+ * http://docs.spring.io/spring-batch/apidocs/org/springframework/batch/test/StepRunner.html
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {JobOptions.class, CommonConfiguration.class})
@@ -44,7 +51,26 @@ public class PedLoaderStepTest {
         ReflectionTestUtils.setField(pedLoaderStep, "jobOptions", jobOptions);
         RepeatStatus status = pedLoaderStep.execute(stepContribution, chunkContext);
 
+        Pedigree pedigree = pedLoaderStep.getPedigree();
+
         assertTrue(status.equals(RepeatStatus.FINISHED));
+
+        //check that Pedigree.Individuals is correctly populated
+        assertEquals(4, pedigree.getIndividuals().size());
+        Individual individualNA19660 = pedigree.getIndividuals().get("NA19660");
+        assertTrue(individualNA19660.getFamily().equals("FAM"));
+        assertTrue(individualNA19660.getSex().equals("2"));
+        assertEquals(Sex.FEMALE, individualNA19660.getSexCode());
+        assertTrue(individualNA19660.getPhenotype().equals("1"));
+        assertEquals(Condition.UNAFFECTED, individualNA19660.getCondition());
+        assertEquals(2, individualNA19660.getChildren().size());
+        assertEquals(Sets.newHashSet("NA19600", "NA19685"),
+                individualNA19660.getChildren().stream().map(Individual::getId).collect(Collectors.toSet()));
+
+        //check that Pedigree.Families is correctly populated
+        assertEquals(1, pedigree.getFamilies().size());
+        assertTrue(pedigree.getFamilies().containsKey("FAM"));
+        assertEquals(4, pedigree.getFamilies().get("FAM").size());
     }
 
     @Before
@@ -54,7 +80,7 @@ public class PedLoaderStepTest {
 
         jobOptions.loadArgs();
 
-        String pedigreeFile = PedLoaderStepTest.class.getResource("/integrated_call_samples.20101123.ped.txt").getFile();
+        String pedigreeFile = PedLoaderStepTest.class.getResource("/ped/pedigree-test-file.ped").getFile();
         jobOptions.getPipelineOptions().put("input.pedigree", pedigreeFile);
     }
 
