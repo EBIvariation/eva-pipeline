34d96d8df9e71fc161f2e68d5467e18f2204a818
jose miguel mut <jomutlo@gmail.com>
2016-11-09 14:43:53 +0000

    VcfReader: store metadata as objects, not strings

--- src/main/java/uk/ac/ebi/eva/pipeline/io/readers/VcfReader.java
+++ src/main/java/uk/ac/ebi/eva/pipeline/io/readers/VcfReader.java
@@ -15,31 +15,45 @@
  */
 package uk.ac.ebi.eva.pipeline.io.readers;
 
+import htsjdk.variant.vcf.VCFCompoundHeaderLine;
+import htsjdk.variant.vcf.VCFConstants;
+import htsjdk.variant.vcf.VCFContigHeaderLine;
 import htsjdk.variant.vcf.VCFFileReader;
+import htsjdk.variant.vcf.VCFFilterHeaderLine;
+import htsjdk.variant.vcf.VCFFormatHeaderLine;
 import htsjdk.variant.vcf.VCFHeader;
 import htsjdk.variant.vcf.VCFHeaderLine;
-import org.opencb.biodata.formats.variant.vcf4.io.VariantVcfReader;
+import htsjdk.variant.vcf.VCFInfoHeaderLine;
+import htsjdk.variant.vcf.VCFSimpleHeaderLine;
+import org.opencb.biodata.formats.variant.vcf4.VcfAlternateHeader;
+import org.opencb.biodata.formats.variant.vcf4.VcfFilterHeader;
+import org.opencb.biodata.formats.variant.vcf4.VcfFormatHeader;
+import org.opencb.biodata.formats.variant.vcf4.VcfInfoHeader;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.biodata.models.variant.VariantSource;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+
 import uk.ac.ebi.eva.pipeline.io.GzipLazyResource;
 import uk.ac.ebi.eva.pipeline.io.mappers.VcfLineMapper;
 import uk.ac.ebi.eva.utils.CompressionHelper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * VCF file reader.
- *
- * This Reader uses a {@link VcfLineMapper} to parse each line, and {@link VariantVcfReader} to fill the VariantSource.
- *
+ * <p>
+ * This Reader uses a {@link VcfLineMapper} to parse each line, and {@link VCFFileReader} from htslib to fill the
+ * VariantSource with metadata from the VCF header.
+ * <p>
  * It doesn't matter if the file is compressed or not.
- *
- * @author Jose Miguel Mut Lopez &lt;jmmut@ebi.ac.uk&gt;
  */
 public class VcfReader extends FlatFileItemReader<List<Variant>> {
     /**
@@ -51,6 +65,7 @@ public class VcfReader extends FlatFileItemReader<List<Variant>> {
     public static final String VARIANT_FILE_HEADER_KEY = "variantFileHeader";
 
     private final VariantSource source;
+
     private final File file;
 
     public VcfReader(VariantSource source, File file) throws IOException {
@@ -78,34 +93,149 @@ public class VcfReader extends FlatFileItemReader<List<Variant>> {
 
     /**
      * Before using the factory inside the mapper, we need to fill some attributes from the header:
-     * - the sample names.
-     * - common vcf fields (FORMAT, INFO, ALT, FILTER, contig)
-     * - other fields (maybe custom fields from users: reference, source...)
-     *
+     * <ul>
+     * <li>common vcf fields (FORMAT, INFO, ALT, FILTER, contig)</li>
+     * <li>other fields (maybe custom fields from users: reference, source...)</li>
+     * <li>the sample names.</li>
+     * <li>The full header string.</li>
+     * </ul>
+     * <p>
      * We get the fields with htsjdk to parse the header. As some tags will appear more than once (INFO, contig, ALT...)
      * we store a map, where the key is the field tag, and the value is a list of lines:
-     *     INFO -> ["##INFO=...ID=CIEND...", "##INFO=...ID=CIPOS..."]
+     * <p>
+     * {@code INFO -> [{ "id" : "CIEND", "number" : "2", "type" : "Integer", "description" : "Confidence..." }, ... ]}
+     * <p>
+     * We are breaking retrocompatibility here, since the previous structure was wrong. In fields that are different but
+     * start with the same key, only the last line was stored, e.g.: {@literal "ALT" : "ID=CN124,Description=...\>"}.
+     * Now ALT would map to a list of deconstructed objects: {@code ALT -> [ {id, description}, ...] }
+     * <p>
+     * Look at the test to see how is this checked.
      */
     private void prepareVariantSource() throws IOException {
         VCFFileReader reader = new VCFFileReader(file, false);
         VCFHeader fileHeader = reader.getFileHeader();
 
-        Map<String, List<String>> metadata = new TreeMap<>();
+        Map<String, List<VCFHeaderLine>> metadata = new TreeMap<>();
         Set<VCFHeaderLine> metaDataInSortedOrder = fileHeader.getMetaDataInSortedOrder();
+        // group the lines by key (INFO, ALT, contig, ...) to a List<VCFHeaderLine>
         for (VCFHeaderLine vcfHeaderLine : metaDataInSortedOrder) {
             if (!metadata.containsKey(vcfHeaderLine.getKey())) {
                 metadata.put(vcfHeaderLine.getKey(), new ArrayList<>());
             }
-            metadata.get(vcfHeaderLine.getKey()).add(vcfHeaderLine.toString());
+            metadata.get(vcfHeaderLine.getKey()).add(vcfHeaderLine);
         }
 
-        for (Map.Entry<String, List<String>> stringListEntry : metadata.entrySet()) {
-            source.addMetadata(stringListEntry.getKey(), stringListEntry.getValue());
+        // for each key, transform into biodata's header lines; unless it's an unsupported field and then write strings
+        for (Map.Entry<String, List<VCFHeaderLine>> headerLineListEntry : metadata.entrySet()) {
+            if (headerLineListEntry.getKey().equals("INFO")) {
+                source.getMetadata().put(headerLineListEntry.getKey(), getVcfInfoHeaders(headerLineListEntry));
+            } else if (headerLineListEntry.getKey().equals("FILTER")) {
+                source.getMetadata().put(headerLineListEntry.getKey(), getVcfFilterHeaders(headerLineListEntry));
+            } else if (headerLineListEntry.getKey().equals("FORMAT")) {
+                source.getMetadata().put(headerLineListEntry.getKey(), getVcfFormatHeaders(headerLineListEntry));
+            } else if (headerLineListEntry.getKey().equals("ALT")) {
+                source.getMetadata().put(headerLineListEntry.getKey(), getVcfAlternateHeaders(headerLineListEntry));
+            } else if (headerLineListEntry.getKey().equals(VCFConstants.CONTIG_HEADER_KEY)) {
+                source.getMetadata().put(headerLineListEntry.getKey(), getContigHeaderLines(headerLineListEntry));
+            } else {
+                source.addMetadata(headerLineListEntry.getKey(), headerLineListEntry.getValue());
+            }
         }
+
         source.setSamples(fileHeader.getGenotypeSamples());
         reader.close();
 
         source.addMetadata(VARIANT_FILE_HEADER_KEY, getHeader(metaDataInSortedOrder));
+    }
+
+    private List<String> getContigHeaderLines(Map.Entry<String, List<VCFHeaderLine>> headerLineListEntry) {
+        List<String> vcfHeaders = new ArrayList<>();
+        for (VCFHeaderLine vcfHeaderLine : headerLineListEntry.getValue()) {
+            if (vcfHeaderLine instanceof VCFContigHeaderLine) {
+                VCFContigHeaderLine line = ((VCFContigHeaderLine) vcfHeaderLine);
+                String biodataLine = line.getID() + line.getValue();
+                vcfHeaders.add(biodataLine);
+            }
+        }
+        return vcfHeaders;
+    }
+
+    private List<VcfAlternateHeader> getVcfAlternateHeaders(Map.Entry<String, List<VCFHeaderLine>> headerLineListEntry) {
+        List<VcfAlternateHeader> vcfHeaders = new ArrayList<>();
+        for (VCFHeaderLine vcfHeaderLine : headerLineListEntry.getValue()) {
+            if (vcfHeaderLine instanceof VCFSimpleHeaderLine) {
+                VCFSimpleHeaderLine line = ((VCFSimpleHeaderLine) vcfHeaderLine);
+                VcfAlternateHeader biodataLine = new VcfAlternateHeader(line.toString());
+                vcfHeaders.add(biodataLine);
+            }
+        }
+        return vcfHeaders;
+    }
+
+    private List<VcfFormatHeader> getVcfFormatHeaders(Map.Entry<String, List<VCFHeaderLine>> headerLineListEntry) {
+        List<VcfFormatHeader> vcfHeaders = new ArrayList<>();
+        for (VCFHeaderLine vcfHeaderLine : headerLineListEntry.getValue()) {
+            if (vcfHeaderLine instanceof VCFFormatHeaderLine) {
+                VCFFormatHeaderLine line = ((VCFFormatHeaderLine) vcfHeaderLine);
+                VcfFormatHeader biodataLine = new VcfFormatHeader(line.getID(), getNumber(line),
+                                                                  line.getType().toString(),
+                                                                  line.getDescription());
+                vcfHeaders.add(biodataLine);
+            }
+        }
+        return vcfHeaders;
+    }
+
+    private List<VcfFilterHeader> getVcfFilterHeaders(Map.Entry<String, List<VCFHeaderLine>> headerLineListEntry) {
+        List<VcfFilterHeader> vcfHeaders = new ArrayList<>();
+        for (VCFHeaderLine vcfHeaderLine : headerLineListEntry.getValue()) {
+            if (vcfHeaderLine instanceof VCFFilterHeaderLine) {
+                VCFFilterHeaderLine line = ((VCFFilterHeaderLine) vcfHeaderLine);
+                VcfFilterHeader biodataLine = new VcfFilterHeader(line.toString());
+                vcfHeaders.add(biodataLine);
+            }
+        }
+        return vcfHeaders;
+    }
+
+    private List<VcfInfoHeader> getVcfInfoHeaders(Map.Entry<String, List<VCFHeaderLine>> headerLineListEntry) {
+        List<VcfInfoHeader> vcfHeaders = new ArrayList<>();
+        for (VCFHeaderLine vcfHeaderLine : headerLineListEntry.getValue()) {
+            if (vcfHeaderLine instanceof VCFInfoHeaderLine) {
+                VCFInfoHeaderLine line = ((VCFInfoHeaderLine) vcfHeaderLine);
+                VcfInfoHeader biodataLine = new VcfInfoHeader(line.getID(), getNumber(line), line.getType().toString(),
+                                                              line.getDescription());
+                vcfHeaders.add(biodataLine);
+            }
+        }
+        return vcfHeaders;
+    }
+
+    public String getNumber(VCFCompoundHeaderLine line) {
+        String number;
+        switch (line.getCountType()) {
+            case A:
+                number = VCFConstants.PER_ALTERNATE_COUNT;
+                break;
+            case R:
+                number = VCFConstants.PER_ALLELE_COUNT;
+                break;
+            case G:
+                number = VCFConstants.PER_GENOTYPE_COUNT;
+                break;
+            case UNBOUNDED:
+                number = VCFConstants.UNBOUNDED_ENCODING_v4;
+                break;
+            case INTEGER:
+                number = Integer.toString(line.getCount());
+                break;
+            default:
+                throw new UnsupportedOperationException(
+                        "VcfReader seems outdated (parsing the header), it doesn't support "
+                                + line.getCountType() .toString()
+                                + " as VCF metadata count specifier (as member of the enum VCFHeaderLineCount)");
+        }
+        return number;
     }
 
     /**
--- src/test/java/uk/ac/ebi/eva/pipeline/io/readers/VcfReaderTest.java
+++ src/test/java/uk/ac/ebi/eva/pipeline/io/readers/VcfReaderTest.java
@@ -1,5 +1,10 @@
 package uk.ac.ebi.eva.pipeline.io.readers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.util.JSON;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -10,22 +15,25 @@ import org.opencb.biodata.models.variant.VariantStudy;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.batch.test.MetaDataInstanceFactory;
+
 import uk.ac.ebi.eva.test.utils.JobTestUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
  * {@link VcfReader}
  * input: a Vcf file
  * output: a list of variants each time its `.read()` is called
- *
- * @author Jose Miguel Mut Lopez &lt;jmmut@ebi.ac.uk&gt;
  */
 public class VcfReaderTest {
 
@@ -43,12 +51,8 @@ public class VcfReaderTest {
         String fileId = "5";
         String studyId = "7";
         String studyName = "study name";
-        VariantSource source = new VariantSource(input,
-                fileId,
-                studyId,
-                studyName,
-                VariantStudy.StudyType.COLLECTION,
-                VariantSource.Aggregation.NONE);
+        VariantSource source = new VariantSource(input, fileId, studyId, studyName, VariantStudy.StudyType.COLLECTION,
+                                                 VariantSource.Aggregation.NONE);
 
         VcfReader vcfReader = new VcfReader(source, input);
         vcfReader.setSaveState(false);
@@ -68,12 +72,8 @@ public class VcfReaderTest {
         String fileId = "5";
         String studyId = "7";
         String studyName = "study name";
-        VariantSource source = new VariantSource(inputFile,
-                fileId,
-                studyId,
-                studyName,
-                VariantStudy.StudyType.COLLECTION,
-                VariantSource.Aggregation.NONE);
+        VariantSource source = new VariantSource(inputFile, fileId, studyId, studyName,
+                                                 VariantStudy.StudyType.COLLECTION, VariantSource.Aggregation.NONE);
 
         VcfReader vcfReader = new VcfReader(source, inputFile);
         vcfReader.setSaveState(false);
@@ -99,18 +99,87 @@ public class VcfReaderTest {
         String fileId = "5";
         String studyId = "7";
         String studyName = "study name";
-        VariantSource source = new VariantSource(tempFile.getAbsolutePath(),
-                fileId,
-                studyId,
-                studyName,
-                VariantStudy.StudyType.COLLECTION,
-                VariantSource.Aggregation.NONE);
+        VariantSource source = new VariantSource(tempFile.getAbsolutePath(), fileId, studyId, studyName,
+                                                 VariantStudy.StudyType.COLLECTION, VariantSource.Aggregation.NONE);
 
         VcfReader vcfReader = new VcfReader(source, tempFile);
         vcfReader.setSaveState(false);
         vcfReader.open(executionContext);
 
         consumeReader(inputFile, source, vcfReader);
+    }
+
+    /**
+     * This test is intended to check that the metadata of the VariantSource is properly filled when using a VcfReader,
+     * in a way that mostly maintains the structure we already have in mongo, in the files collection. See the doc in
+     * {@link VcfReader#prepareVariantSource()}
+     * @throws Exception
+     */
+    @Test
+    public void testConversion() throws Exception {
+
+        ExecutionContext executionContext = MetaDataInstanceFactory.createStepExecution().getExecutionContext();
+
+        // uncompress the input VCF into a temporal file
+        final String inputFilePath = "/small20.vcf.gz";
+        String inputFile = VcfReaderTest.class.getResource(inputFilePath).getFile();
+        File tempFile = JobTestUtils.createTempFile();
+        JobTestUtils.uncompress(inputFile, tempFile);
+
+        String fileId = "5";
+        String studyId = "7";
+        String studyName = "study name";
+        VariantSource source = new VariantSource(tempFile.getAbsolutePath(), fileId, studyId, studyName,
+                                                 VariantStudy.StudyType.COLLECTION, VariantSource.Aggregation.NONE);
+
+        VcfReader vcfReader = new VcfReader(source, tempFile);
+        vcfReader.setSaveState(false);
+        vcfReader.open(executionContext);
+
+        consumeReader(inputFile, source, vcfReader);
+
+        char CHARACTER_TO_REPLACE_DOTS = (char) 163;
+        Map<String, Object> meta = source.getMetadata();
+        BasicDBObject metadataMongo = new BasicDBObject();
+        for (Map.Entry<String, Object> metaEntry : meta.entrySet()) {
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectWriter writer = mapper.writer();
+            String key = metaEntry.getKey().replace('.', CHARACTER_TO_REPLACE_DOTS);
+            Object value = metaEntry.getValue();
+            String jsonString = writer.writeValueAsString(value);
+            metadataMongo.append(key, JSON.parse(jsonString));
+        }
+        checkFieldsInsideList(metadataMongo, "INFO", Arrays.asList("id", "description", "number", "type"));
+        checkFieldsInsideList(metadataMongo, "FORMAT", Arrays.asList("id", "description", "number", "type"));
+        checkFieldsInsideList(metadataMongo, "ALT", Arrays.asList("id", "description"));
+        checkFieldsInsideList(metadataMongo, "FILTER", Arrays.asList("id", "description"));
+        checkStringInsideList(metadataMongo, "contig");
+    }
+
+    private void checkStringInsideList(BasicDBObject metadataMongo, String field) {
+        assertTrue(metadataMongo.containsField(field));
+        Object objectList = metadataMongo.get(field);
+        assertTrue(objectList instanceof BasicDBList);
+        BasicDBList list = (BasicDBList) objectList;
+        for (Object element : list) {
+            assertTrue(element instanceof String);
+            assertNotNull(element);
+            assertFalse(element.toString().isEmpty());
+        }
+    }
+
+    private void checkFieldsInsideList(BasicDBObject metadataMongo, String field, List<String> innerFields) {
+        assertTrue(metadataMongo.containsField(field));
+        Object objectList = metadataMongo.get(field);
+        assertTrue(objectList instanceof BasicDBList);
+        BasicDBList list = (BasicDBList) objectList;
+        for (Object element : list) {
+            assertTrue(element instanceof BasicDBObject);
+            for (String innerField : innerFields) {
+                assertNotNull(((BasicDBObject) element).get(innerField));
+                assertFalse(((BasicDBObject) element).get(innerField).toString().isEmpty());
+            }
+        }
     }
 
     private void consumeReader(String inputFile, VariantSource source, VcfReader vcfReader) throws Exception {
