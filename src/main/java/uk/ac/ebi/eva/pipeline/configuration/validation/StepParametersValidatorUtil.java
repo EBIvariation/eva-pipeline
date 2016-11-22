19271b712fdb756fdf836a3a63e4a8b351e9b393
Diego Poggioli <diego@ebi.ac.uk>
2016-11-22 16:29:54 +0000

    JobParameters checks moved into StepParametersValidatorUtil and using the JobParametersName instead of free text

--- src/main/java/uk/ac/ebi/eva/pipeline/configuration/validation/StepParametersValidatorUtil.java
+++ src/main/java/uk/ac/ebi/eva/pipeline/configuration/validation/StepParametersValidatorUtil.java
@@ -24,59 +24,209 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * Utility to hold the checks on strings, dirs, files... parameters
+ * Utility to hold the checks on {@link org.springframework.batch.core.JobParameters}
+ * and low level checks for strings, dirs, files... parameters
  */
 public class StepParametersValidatorUtil {
 
-    public static void checkNullOrEmptyString(String stringToValidate,
-                                              String message) throws JobParametersInvalidException {
+    /**
+     * Checks that the vep path has been filled in
+     *
+     * @param vepPath           The directory where vep is
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the vep path is null or empty
+     */
+    void validateVepPath(String vepPath, String jobParametersName) throws JobParametersInvalidException {
+        checkFileExistAndReadable(vepPath, jobParametersName);
+    }
+
+    /**
+     * Checks that the cache version option has benn filled in
+     *
+     * @param vepCacheVersion   The version of the vep cache
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the vep cache version is null or empty
+     */
+    void validateVepCacheVersion(String vepCacheVersion,
+                                 String jobParametersName) throws JobParametersInvalidException {
+        checkNullOrEmptyString(vepCacheVersion, jobParametersName);
+    }
+
+    /**
+     * Checks that the path of the vep cache is a valid directory
+     *
+     * @param vepCachePath      The path of the vep cache
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the vep cache path is not a valid directory
+     */
+    void validateVepCachePath(String vepCachePath, String jobParametersName) throws JobParametersInvalidException {
+        checkDirectory(vepCachePath, jobParametersName);
+    }
+
+    /**
+     * Checks that the species of the vep cache has been filled in
+     *
+     * @param vepCacheSpecies   The cache species used by vep
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the vep cache species is null or empty
+     */
+    void validateVepCacheSpecies(String vepCacheSpecies,
+                                 String jobParametersName) throws JobParametersInvalidException {
+        checkNullOrEmptyString(vepCacheSpecies, jobParametersName);
+    }
+
+    /**
+     * Checks that the fasta input file exist and is readable
+     *
+     * @param inputFasta        Path to the fasta file used by vep
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the file is not a valid path, does not exist or is not readable
+     */
+    void validateInputFasta(String inputFasta, String jobParametersName) throws JobParametersInvalidException {
+        checkFileExistAndReadable(inputFasta, jobParametersName);
+    }
+
+    /**
+     * Checks that the number of forks is a valid integer number
+     *
+     * @param vepNumForks       The number of fork to use by vep
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the number of fork is not a valid number
+     */
+    void validateVepNumForks(String vepNumForks, String jobParametersName) throws JobParametersInvalidException {
+        checkInteger(vepNumForks, jobParametersName);
+    }
+
+    /**
+     * Checks that the output directory for annotations is a directory and it is writable
+     *
+     * @param outputDirAnnotation Output directory to store VEP annotations
+     * @param jobParametersName
+     * @throws JobParametersInvalidException Id the output directory is not a directory
+     */
+    void validateOutputDirAnnotation(String outputDirAnnotation,
+                                     String jobParametersName) throws JobParametersInvalidException {
+        checkDirectory(outputDirAnnotation, jobParametersName);
+    }
+
+    /**
+     * Checks that the VCF id has been filled in.
+     *
+     * @param inputVcfId
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the VCF id is null or empty
+     */
+    void validateInputVcfId(String inputVcfId, String jobParametersName) throws JobParametersInvalidException {
+        checkNullOrEmptyString(inputVcfId, jobParametersName);
+    }
+
+    /**
+     * Checks that the study id has been filled in.
+     *
+     * @param inputStudyId
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the study id is null or empty
+     */
+    void validateInputStudyId(String inputStudyId, String jobParametersName) throws JobParametersInvalidException {
+        checkNullOrEmptyString(inputStudyId, jobParametersName);
+    }
+
+    /**
+     * Checks that the database name has been filled in.
+     *
+     * @param dbName            The database name
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the database name is null or empty
+     */
+    void validateDbName(String dbName, String jobParametersName) throws JobParametersInvalidException {
+        checkNullOrEmptyString(dbName, jobParametersName);
+    }
+
+    /**
+     * Checks that the name of the variants collection has been filled in.
+     *
+     * @param collectionsVariantsName The name of the variants collection
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the variant collection name is null or empty
+     */
+    void validateDbCollectionsVariantsName(String collectionsVariantsName,
+                                           String jobParametersName) throws JobParametersInvalidException {
+        checkNullOrEmptyString(collectionsVariantsName, jobParametersName);
+    }
+
+    /**
+     * Checks that the option to allow restartability has been filled in and it is "true" or "false".
+     *
+     * @param configRestartabilityAllow Boolean option to allow restartability of a {@link org.springframework.batch.core.Step}
+     * @param jobParametersName
+     * @throws JobParametersInvalidException If the allow restartability option is null or empty or any text different
+     *                                       from 'true' or 'false'
+     */
+    void validateConfigRestartabilityAllow(String configRestartabilityAllow,
+                                           String jobParametersName) throws JobParametersInvalidException {
+        checkNullOrEmptyString(configRestartabilityAllow, jobParametersName);
+        checkBooleanStringSyntax(configRestartabilityAllow, jobParametersName);
+    }
+
+
+    private void checkNullOrEmptyString(String stringToValidate,
+                                        String jobParametersName) throws JobParametersInvalidException {
         if (Strings.isNullOrEmpty(stringToValidate)) {
-            throw new JobParametersInvalidException(message);
+            throw new JobParametersInvalidException(
+                    String.format("%s in %s must be specified", stringToValidate, jobParametersName));
         }
     }
 
-    public static void checkBooleanStringSyntax(String booleanStringToValidate,
-                                                String message) throws JobParametersInvalidException {
+    private void checkBooleanStringSyntax(String booleanStringToValidate,
+                                          String jobParametersName) throws JobParametersInvalidException {
         if (!booleanStringToValidate.equalsIgnoreCase("true") && !booleanStringToValidate.equalsIgnoreCase("false")) {
-            throw new JobParametersInvalidException(message);
+            throw new JobParametersInvalidException(
+                    String.format("%s in %s must be 'true' or 'false'", booleanStringToValidate, jobParametersName));
         }
     }
 
-    public static void checkDirectory(String dirToValidate, String message) throws JobParametersInvalidException {
+    private void checkDirectory(String dirToValidate, String jobParametersName) throws JobParametersInvalidException {
         Path path;
         try {
             path = Paths.get(dirToValidate);
         } catch (InvalidPathException e) {
-            throw new JobParametersInvalidException(e.getMessage());
+            throw new JobParametersInvalidException(
+                    String.format("%s in %s is not a valid path", dirToValidate, jobParametersName));
         }
 
         if (!Files.isDirectory(path)) {
-            throw new JobParametersInvalidException(message);
+            throw new JobParametersInvalidException(
+                    String.format("%s in %s is not a valid directory", dirToValidate, jobParametersName));
         }
     }
 
-    public static void checkFileExistAndReadable(String fileToValidate) throws JobParametersInvalidException {
+    private void checkFileExistAndReadable(String fileToValidate,
+                                           String jobParametersName) throws JobParametersInvalidException {
         Path path;
         try {
             path = Paths.get(fileToValidate);
         } catch (InvalidPathException e) {
-            throw new JobParametersInvalidException(e.getMessage());
+            throw new JobParametersInvalidException(
+                    String.format("File %s in %s is not a valid path", fileToValidate, jobParametersName));
         }
 
         if (Files.notExists(path)) {
-            throw new JobParametersInvalidException(String.format("File %s does not exist", fileToValidate));
+            throw new JobParametersInvalidException(
+                    String.format("File %s in %s does not exist", fileToValidate, jobParametersName));
         }
 
         if (!Files.isReadable(path)) {
-            throw new JobParametersInvalidException(String.format("File %s is not readable", fileToValidate));
+            throw new JobParametersInvalidException(
+                    String.format("File %s in %s is not readable", fileToValidate, jobParametersName));
         }
     }
 
-    public static void checkInteger(String numberToValidate, String message) throws JobParametersInvalidException {
+    private void checkInteger(String numberToValidate, String jobParametersName) throws JobParametersInvalidException {
         try {
             Integer.parseInt(numberToValidate);
         } catch (NumberFormatException e) {
-            throw new JobParametersInvalidException(message);
+            throw new JobParametersInvalidException(
+                    String.format("%s in %s is not a valid number", numberToValidate, jobParametersName));
         }
     }
 }
--- src/main/java/uk/ac/ebi/eva/pipeline/configuration/validation/VepAnnotationGeneratorStepParametersValidator.java
+++ src/main/java/uk/ac/ebi/eva/pipeline/configuration/validation/VepAnnotationGeneratorStepParametersValidator.java
@@ -28,127 +28,41 @@ import uk.ac.ebi.eva.pipeline.configuration.JobParametersNames;
  */
 public class VepAnnotationGeneratorStepParametersValidator extends DefaultJobParametersValidator {
 
+    private StepParametersValidatorUtil parametersValidator;
+
     public VepAnnotationGeneratorStepParametersValidator() {
-        super(new String[]{JobParametersNames.APP_VEP_PATH, JobParametersNames.APP_VEP_CACHE_VERSION,
-                      JobParametersNames.APP_VEP_CACHE_PATH, JobParametersNames.APP_VEP_CACHE_SPECIES,
-                      JobParametersNames.INPUT_FASTA, JobParametersNames.APP_VEP_NUMFORKS,
-                      JobParametersNames.OUTPUT_DIR_ANNOTATION, JobParametersNames.INPUT_STUDY_ID,
-                      JobParametersNames.INPUT_VCF_ID},
+        super(new String[]{JobParametersNames.APP_VEP_CACHE_PATH, JobParametersNames.APP_VEP_CACHE_SPECIES,
+                      JobParametersNames.APP_VEP_CACHE_VERSION, JobParametersNames.APP_VEP_NUMFORKS,
+                      JobParametersNames.APP_VEP_PATH, JobParametersNames.INPUT_FASTA, JobParametersNames.INPUT_STUDY_ID,
+                      JobParametersNames.INPUT_VCF_ID, JobParametersNames.OUTPUT_DIR_ANNOTATION,
+              },
               new String[]{});
+
+        parametersValidator = new StepParametersValidatorUtil();
     }
 
     @Override
     public void validate(JobParameters parameters) throws JobParametersInvalidException {
         super.validate(parameters);
 
-        validateVepPath(parameters.getString(JobParametersNames.APP_VEP_PATH));
-        validateVepCacheVersion(parameters.getString(JobParametersNames.APP_VEP_CACHE_VERSION));
-        validateVepCachePath(parameters.getString(JobParametersNames.APP_VEP_CACHE_PATH));
-        validateVepCacheSpecies(parameters.getString(JobParametersNames.APP_VEP_CACHE_SPECIES));
-        validateInputFasta(parameters.getString(JobParametersNames.INPUT_FASTA));
-        validateVepNumForks(parameters.getString(JobParametersNames.APP_VEP_NUMFORKS));
-
-        //// TODO: the following validations are shared with VepInputGeneratorStepParametersValidator
-        validateOutputDirAnnotation(parameters.getString(JobParametersNames.OUTPUT_DIR_ANNOTATION));
-        validateInputStudyId(parameters.getString(JobParametersNames.INPUT_STUDY_ID));
-        validateInputVcfId(parameters.getString(JobParametersNames.INPUT_VCF_ID));
-    }
-
-    /**
-     * Checks that the vep path has been filled in
-     *
-     * @param vepPath The directory where vep is
-     * @throws JobParametersInvalidException If the vep path is null or empty
-     */
-    void validateVepPath(String vepPath) throws JobParametersInvalidException {
-        StepParametersValidatorUtil.checkFileExistAndReadable(vepPath);
-    }
-
-    /**
-     * Checks that the cache version option has benn filled in
-     *
-     * @param vepCacheVersion The version of the vep cache
-     * @throws JobParametersInvalidException If the vep cache version is null or empty
-     */
-    void validateVepCacheVersion(String vepCacheVersion) throws JobParametersInvalidException {
-        StepParametersValidatorUtil.checkNullOrEmptyString(vepCacheVersion,
-                                                           "A cache version for vep must be specified");
-    }
-
-    /**
-     * Checks that the path of the vep cache is a valid directory
-     *
-     * @param vepCachePath The path of the vep cache
-     * @throws JobParametersInvalidException If the vep cache path is not a valid directory
-     */
-    void validateVepCachePath(String vepCachePath) throws JobParametersInvalidException {
-        StepParametersValidatorUtil.checkDirectory(vepCachePath, String.format(
-                "The vep cache path provided %s is not valid", vepCachePath));
-    }
-
-    /**
-     * Checks that the species of the vep cache has been filled in
-     *
-     * @param vepCacheSpecies The cache species used by vep
-     * @throws JobParametersInvalidException If the vep cache species is null or empty
-     */
-    void validateVepCacheSpecies(String vepCacheSpecies) throws JobParametersInvalidException {
-        StepParametersValidatorUtil.checkNullOrEmptyString(vepCacheSpecies, "Species option for vep must be specified");
-    }
-
-    /**
-     * Checks that the fasta input file exist and is readable
-     *
-     * @param inputFasta Path to the fasta file used by vep
-     * @throws JobParametersInvalidException If the file is not a valid path, does not exist or is not readable
-     */
-    void validateInputFasta(String inputFasta) throws JobParametersInvalidException {
-        StepParametersValidatorUtil.checkFileExistAndReadable(inputFasta);
-    }
-
-    /**
-     * Checks that the number of forks is a valid integer number
-     *
-     * @param vepNumForks The number of fork to use by vep
-     * @throws JobParametersInvalidException If the number of fork is not a valid number
-     */
-    void validateVepNumForks(String vepNumForks) throws JobParametersInvalidException {
-        StepParametersValidatorUtil.checkInteger(vepNumForks, String.format("%s is not a valid number of forks for vep",
-                                                                            vepNumForks));
-    }
-
-
-    //// TODO: the following validations are shared with VepInputGeneratorStepParametersValidator
-
-    /**
-     * Checks that the output directory for annotations is a directory and it is writable
-     *
-     * @param outputDirAnnotation Output directory to store VEP annotations
-     * @throws JobParametersInvalidException Id the output directory is not a directory
-     */
-    void validateOutputDirAnnotation(String outputDirAnnotation) throws JobParametersInvalidException {
-        StepParametersValidatorUtil
-                .checkDirectory(outputDirAnnotation, "The output directory for annotations is not valid");
-    }
-
-    /**
-     * Checks that the vcf id has been filled in.
-     *
-     * @param inputVcfId
-     * @throws JobParametersInvalidException If the vcf id is null or empty
-     */
-    void validateInputVcfId(String inputVcfId) throws JobParametersInvalidException {
-        StepParametersValidatorUtil.checkNullOrEmptyString(inputVcfId, "A vcf id name must be specified");
-    }
-
-    /**
-     * Checks that the study id has been filled in.
-     *
-     * @param inputStudyId
-     * @throws JobParametersInvalidException If the study id is null or empty
-     */
-    void validateInputStudyId(String inputStudyId) throws JobParametersInvalidException {
-        StepParametersValidatorUtil.checkNullOrEmptyString(inputStudyId, "A study ID name must be specified");
+        parametersValidator.validateVepPath(parameters.getString(JobParametersNames.APP_VEP_PATH),
+                                            JobParametersNames.APP_VEP_PATH);
+        parametersValidator.validateVepCacheVersion(parameters.getString(JobParametersNames.APP_VEP_CACHE_VERSION),
+                                                    JobParametersNames.APP_VEP_CACHE_VERSION);
+        parametersValidator.validateVepCachePath(parameters.getString(JobParametersNames.APP_VEP_CACHE_PATH),
+                                                 JobParametersNames.APP_VEP_CACHE_PATH);
+        parametersValidator.validateVepCacheSpecies(parameters.getString(JobParametersNames.APP_VEP_CACHE_SPECIES),
+                                                    JobParametersNames.APP_VEP_CACHE_SPECIES);
+        parametersValidator.validateInputFasta(parameters.getString(JobParametersNames.INPUT_FASTA),
+                                               JobParametersNames.INPUT_FASTA);
+        parametersValidator.validateVepNumForks(parameters.getString(JobParametersNames.APP_VEP_NUMFORKS),
+                                                JobParametersNames.APP_VEP_NUMFORKS);
+        parametersValidator.validateOutputDirAnnotation(parameters.getString(JobParametersNames.OUTPUT_DIR_ANNOTATION),
+                                                        JobParametersNames.OUTPUT_DIR_ANNOTATION);
+        parametersValidator.validateInputStudyId(parameters.getString(JobParametersNames.INPUT_STUDY_ID),
+                                                 JobParametersNames.INPUT_STUDY_ID);
+        parametersValidator.validateInputVcfId(parameters.getString(JobParametersNames.INPUT_VCF_ID),
+                                               JobParametersNames.INPUT_VCF_ID);
     }
 
 }
