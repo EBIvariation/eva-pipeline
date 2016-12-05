549418fe1e6e7b399f58d84fd500013bdc4cbb61
jose miguel mut <jomutlo@gmail.com>
2016-12-05 15:49:42 +0000

    manual test emulation in automated test. finally!

--- src/test/java/uk/ac/ebi/eva/pipeline/ApplicationTest.java
+++ src/test/java/uk/ac/ebi/eva/pipeline/ApplicationTest.java
@@ -1,10 +1,20 @@
 package uk.ac.ebi.eva.pipeline;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import uk.ac.ebi.eva.pipeline.jobs.GenotypedVcfJob;
+
+import java.util.List;
 
 /**
  * The purpose of this test is to imitate an execution made by an user through the CLI.
@@ -17,8 +27,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles({"integrationTest,test,mongo"})
 public class ApplicationTest {
 
+    @Autowired
+    JobExplorer jobExplorer;
+
     @Test
     public void main() throws Exception {
+        Assert.assertEquals(1, jobExplorer.getJobNames().size());
+        Assert.assertEquals(GenotypedVcfJob.jobName, jobExplorer.getJobNames().get(0));
+        
+        List<JobInstance> jobInstances = jobExplorer.getJobInstances(GenotypedVcfJob.jobName, 0, 100);
+        Assert.assertEquals(1, jobInstances.size());
+        
+        JobExecution jobExecution = jobExplorer.getJobExecution(jobInstances.get(0).getInstanceId());
+        Assert.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
     }
 
 }
