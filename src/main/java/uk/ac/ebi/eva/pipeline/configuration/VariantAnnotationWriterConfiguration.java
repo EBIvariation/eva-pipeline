345bb4db850a5f3e2fe7ba3e76f9a4326e919bc4
jorizci <jorizci@gmail.com>
2016-12-14 14:47:55 +0000

    Added stepscope  to VariantAnnotationWriterConfiguration.java

--- src/main/java/uk/ac/ebi/eva/pipeline/configuration/VariantAnnotationWriterConfiguration.java
+++ src/main/java/uk/ac/ebi/eva/pipeline/configuration/VariantAnnotationWriterConfiguration.java
@@ -1,12 +1,12 @@
 package uk.ac.ebi.eva.pipeline.configuration;
 
 import org.opencb.biodata.models.variant.annotation.VariantAnnotation;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.mongodb.core.MongoOperations;
 import uk.ac.ebi.eva.pipeline.Application;
 import uk.ac.ebi.eva.pipeline.io.writers.VepAnnotationMongoWriter;
@@ -23,7 +23,7 @@ public class VariantAnnotationWriterConfiguration {
     private JobOptions jobOptions;
 
     @Bean
-    @Scope("prototype")
+    @StepScope
     @Profile(Application.VARIANT_ANNOTATION_MONGO_PROFILE)
     public ItemWriter<VariantAnnotation> variantAnnotationItemWriter() throws UnknownHostException {
         MongoOperations mongoOperations = MongoDBHelper.getMongoOperations(jobOptions
