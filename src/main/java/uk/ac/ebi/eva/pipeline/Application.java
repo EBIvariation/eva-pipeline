a5773fa49cec3351fbfa56b4806abf36a383f729
jorizci <jorizci@gmail.com>
2016-12-16 09:38:27 +0000

    Fix typo.

--- src/main/java/uk/ac/ebi/eva/pipeline/Application.java
+++ src/main/java/uk/ac/ebi/eva/pipeline/Application.java
@@ -36,7 +36,7 @@ public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
     public static final String VARIANT_WRITER_MONGO_PROFILE = "variant-writer-mongo";
     public static final String VARIANT_ANNOTATION_MONGO_PROFILE = "variant-annotation-mongo";
-    public static final String PRODUCTION_PROFILE = "profile";
+    public static final String PRODUCTION_PROFILE = "production";
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
