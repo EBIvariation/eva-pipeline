88fa94d6f720b08568e9420463581c96484d175e
Diego Poggioli <diego@ebi.ac.uk>
2017-04-20 14:46:47 +0100

    Monitor progress of variant annotation load - EVA-719

--- src/main/java/uk/ac/ebi/eva/pipeline/listeners/AnnotationLoaderStepStatisticsListener.java
+++ src/main/java/uk/ac/ebi/eva/pipeline/listeners/AnnotationLoaderStepStatisticsListener.java
@@ -18,25 +18,36 @@ package uk.ac.ebi.eva.pipeline.listeners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 
 import uk.ac.ebi.eva.pipeline.parameters.ExecutionContextParametersNames;
 import uk.ac.ebi.eva.pipeline.parameters.JobParametersNames;
 import uk.ac.ebi.eva.utils.FileWithHeaderNumberOfLinesEstimator;
+import uk.ac.ebi.eva.utils.URLHelper;
 
 /**
- * - Estimate the number of lines in the VCF file before the step. This will be used in {@link StepProgressListener}
+ * - Estimate the number of lines in the VEP annotation file before the step. This will be used in {@link StepProgressListener}
  * - Log a statistics summary after the step
  */
-public class VariantLoaderStepStatisticsListener implements StepExecutionListener {
-    private static final Logger logger = LoggerFactory.getLogger(VariantLoaderStepStatisticsListener.class);
+public class AnnotationLoaderStepStatisticsListener implements StepExecutionListener {
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationLoaderStepStatisticsListener.class);
 
     @Override
     public void beforeStep(StepExecution stepExecution) {
-        String vcfFilePath = stepExecution.getJobExecution().getJobParameters().getString(JobParametersNames.INPUT_VCF);
-        long estimatedTotalNumberOfLines = new FileWithHeaderNumberOfLinesEstimator().estimateNumberOfLines(vcfFilePath);
-        stepExecution.getExecutionContext().put(ExecutionContextParametersNames.NUMBER_OF_LINES, estimatedTotalNumberOfLines);
+        JobParameters parameters = stepExecution.getJobExecution().getJobParameters();
+
+        String outputDirAnnotation = parameters.getString(JobParametersNames.OUTPUT_DIR_ANNOTATION);
+        String studyId = parameters.getString(JobParametersNames.INPUT_STUDY_ID);
+        String fileId = parameters.getString(JobParametersNames.INPUT_VCF_ID);
+
+        String vepAnnotationFilePath = URLHelper.resolveVepOutput(outputDirAnnotation, studyId, fileId);
+
+        long estimatedTotalNumberOfLines = new FileWithHeaderNumberOfLinesEstimator()
+                .estimateNumberOfLines(vepAnnotationFilePath);
+        stepExecution.getExecutionContext()
+                .put(ExecutionContextParametersNames.NUMBER_OF_LINES, estimatedTotalNumberOfLines);
     }
 
     @Override
@@ -47,5 +58,4 @@ public class VariantLoaderStepStatisticsListener implements StepExecutionListene
 
         return null;
     }
-
 }
