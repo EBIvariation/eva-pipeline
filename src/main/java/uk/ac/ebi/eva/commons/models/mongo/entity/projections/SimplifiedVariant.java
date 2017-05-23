bfa3b9bfdb000c890b424f0a2bbd66b5779b1615
jorizci <jorizci@gmail.com>
2017-05-23 15:51:29 +0100

    Added documentation and licenses

--- src/main/java/uk/ac/ebi/eva/commons/models/mongo/entity/projections/SimplifiedVariant.java
+++ src/main/java/uk/ac/ebi/eva/commons/models/mongo/entity/projections/SimplifiedVariant.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.ebi.eva.commons.models.mongo.entity.projections;
 
 import org.springframework.data.annotation.Id;
@@ -23,6 +38,9 @@ import static uk.ac.ebi.eva.commons.models.mongo.entity.VariantDocument.buildVar
 import static uk.ac.ebi.eva.commons.models.mongo.entity.VariantDocument.createHgvsMongo;
 import static uk.ac.ebi.eva.commons.models.mongo.entity.VariantDocument.generateAtField;
 
+/**
+ * Simplified representation of variant to be used when inserting or updating a variant
+ */
 public class SimplifiedVariant {
 
     @Id
