c5c4611335293afa60fa66911832592041439d6a
jorizci <jorizci@gmail.com>
2017-05-23 15:51:29 +0100

    Added documentation and licenses

--- src/main/java/uk/ac/ebi/eva/commons/models/mongo/entity/subdocuments/VariantStatsMongo.java
+++ src/main/java/uk/ac/ebi/eva/commons/models/mongo/entity/subdocuments/VariantStatsMongo.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2017 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.ebi.eva.commons.models.mongo.entity.subdocuments;
 
-import com.mongodb.BasicDBObject;
 import org.opencb.biodata.models.feature.Genotype;
 import org.springframework.data.mongodb.core.mapping.Field;
 import uk.ac.ebi.eva.commons.models.data.VariantStats;
@@ -8,6 +22,9 @@ import uk.ac.ebi.eva.commons.models.data.VariantStats;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Mongo database representation of Variant Stats.
+ */
 public class VariantStatsMongo {
 
     public final static String COHORT_ID = "cid";
