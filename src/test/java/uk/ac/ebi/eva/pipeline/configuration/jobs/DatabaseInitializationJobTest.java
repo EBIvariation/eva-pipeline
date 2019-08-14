e1634eabe09fd656182f89969ecf18e7f2ed6829
junaidnz97 <junaidnz97@gmail.com>
2019-08-14 19:18:29 +0530

    Added license and copyright header

--- src/test/java/uk/ac/ebi/eva/pipeline/configuration/jobs/DatabaseInitializationJobTest.java
+++ src/test/java/uk/ac/ebi/eva/pipeline/configuration/jobs/DatabaseInitializationJobTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.ac.ebi.eva.pipeline.configuration.jobs;
 
 import org.junit.Rule;
@@ -11,7 +27,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.ac.ebi.eva.test.configuration.BatchTestConfiguration;
-import uk.ac.ebi.eva.test.rules.PipelineTemporaryFolderRule;
 import uk.ac.ebi.eva.test.rules.TemporaryMongoRule;
 import uk.ac.ebi.eva.utils.EvaJobParameterBuilder;
 
