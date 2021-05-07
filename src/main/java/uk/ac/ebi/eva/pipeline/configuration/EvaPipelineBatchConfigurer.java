ba2fb04174dd5de27ab67f9dc762923e386ae694
Sundar Venkataraman <sundarvenkata@ebi.ac.uk>
2021-05-07 13:35:20 +0100

    Use Spring Boot 1 repo. job. compat configurer

--- src/main/java/uk/ac/ebi/eva/pipeline/configuration/EvaPipelineBatchConfigurer.java
+++ src/main/java/uk/ac/ebi/eva/pipeline/configuration/EvaPipelineBatchConfigurer.java
@@ -1,48 +1,22 @@
 package uk.ac.ebi.eva.pipeline.configuration;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
-import org.springframework.batch.core.repository.ExecutionContextSerializer;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.dao.XStreamExecutionContextStringSerializer;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uk.ac.ebi.eva.commons.batch.configuration.SpringBoot1CompatibilityConfiguration;
 
 @Configuration
 @EnableBatchProcessing
-public class EvaPipelineBatchConfigurer extends DefaultBatchConfigurer {
-
-    @Autowired
-    private DataSource dataSource;
+public class EvaPipelineBatchConfigurer {
 
     @Bean
-    ExecutionContextSerializer getSerializer() {
-        return new XStreamExecutionContextStringSerializer();
-    }
-
-
-    @Override
-    protected JobRepository createJobRepository() throws Exception {
-        final JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
-        factory.setDataSource(dataSource);
-        factory.setSerializer(getSerializer());
-        factory.setTransactionManager(getTransactionManager());
-        factory.afterPropertiesSet();
-        return factory.getObject();
-    }
-
-    @Override
-    protected JobExplorer createJobExplorer() throws Exception {
-        final JobExplorerFactoryBean jobExplorerFactoryBean = new JobExplorerFactoryBean();
-        jobExplorerFactoryBean.setDataSource(dataSource);
-        jobExplorerFactoryBean.setSerializer(getSerializer());
-        jobExplorerFactoryBean.afterPropertiesSet();
-        return jobExplorerFactoryBean.getObject();
+    public BatchConfigurer configurer(DataSource dataSource, EntityManagerFactory entityManagerFactory)
+            throws Exception {
+        return SpringBoot1CompatibilityConfiguration.getSpringBoot1CompatibleBatchConfigurer(dataSource,
+                entityManagerFactory);
     }
 }
