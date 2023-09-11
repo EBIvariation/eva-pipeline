bd8665d13b02900b076c586abf22c4a140144f5c
nkumar2 <nkumar2@ebi.ac.uk>
2023-09-11 14:26:12 +0100

    add unit test for WriteResultChecking

---
+++ src/test/java/uk/ac/ebi/eva/pipeline/configuration/MongoTemplateTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.ebi.eva.pipeline.configuration;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.ac.ebi.eva.commons.models.metadata.AnnotationMetadata;
+
+import static org.mockito.Mockito.doThrow;
+
+@RunWith(SpringRunner.class)
+public class MongoTemplateTest {
+    @MockBean
+    private MongoTemplate mongoTemplate;
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void testMongoTemplateWriteResultChecking() {
+        AnnotationMetadata annotationMetadata = new AnnotationMetadata("vep_1", "vep_cache_1", true);
+        doThrow(new DataAccessException("Simulated exception") {})
+                .when(mongoTemplate).save(annotationMetadata, "AnnotationMetadata");
+
+        mongoTemplate.save(annotationMetadata, "AnnotationMetadata");
+    }
+}
